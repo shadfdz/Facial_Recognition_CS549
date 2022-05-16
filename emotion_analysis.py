197ec6e68428fc98a5b117d54507cc60cafd10ba
Shad Fernandez <shadfdz@gmail.com>
2022-05-15 22:38:09 -0700

    Update emotion_analysis.py

--- emotion_analysis.py
+++ emotion_analysis.py
@@ -22,28 +22,28 @@ for i in range(10):
     df = pd.read_csv(f_name)
     df = df.loc[:,df.columns!='Unnamed: 0'] # remov unnamed emotions
     df_emotions = df.loc[df.sum(axis=1) != 1,:].reset_index(drop=True) # remove rows with single emotions
-    # df_emotions_rm = df_emotions.rolling(30).mean()
-    #
-    # plt.stackplot(df_emotions_rm.index, df_emotions_rm.Angry, df_emotions_rm.Disgust, df_emotions_rm.Fear,
-    #               df_emotions_rm.Happy, df_emotions_rm.Neutral, df_emotions_rm.Sad, df_emotions_rm.Surprise,
-    #               labels=df_emotions_rm.columns)
-    # plt.legend(loc='upper left')
-    # plt.xlabel('Sec')
-    # plt.ylabel('Rolling Mean')
-    # plt.title('Rolling Mean Every 30 Seconds of Group Emotions Vid {}'.format(i+1))
-    # plt.savefig('./Output/GroupEmotions_{}.png'.format(i+1))
-    # plt.show()
-    # plt.close()
+    df_emotions_rm = df_emotions.rolling(30).mean()
+    
+    plt.stackplot(df_emotions_rm.index, df_emotions_rm.Angry, df_emotions_rm.Disgust, df_emotions_rm.Fear,
+                  df_emotions_rm.Happy, df_emotions_rm.Neutral, df_emotions_rm.Sad, df_emotions_rm.Surprise,
+                  labels=df_emotions_rm.columns)
+    plt.legend(loc='upper left')
+    plt.xlabel('Sec')
+    plt.ylabel('Rolling Mean')
+    plt.title('Rolling Mean Every 30 Seconds of Group Emotions Vid {}'.format(i+1))
+    plt.savefig('./Output/GroupEmotions_{}.png'.format(i+1))
+    plt.show()
+    plt.close()
 
-    # print((df_emotions.sum(axis=0)/df_emotions.sum(axis=0).sum()).to_list())
-    # emotion_totals_list.append((df_emotions.sum(axis=0)/df_emotions.sum(axis=0).sum()).to_list())
+    # append totals to emotion totals list
+    emotion_totals_list.append((df_emotions.sum(axis=0)/df_emotions.sum(axis=0).sum()).to_list())
 
 # create heat map of percent totals of each video
-# df_emotion_totals = pd.DataFrame(emotion_totals_list, columns=df.columns, index=[i+1 for i in range(10)])
-# sns.heatmap(df_emotion_totals, annot = True)
-# plt.title('Percent Total of Emotions for Video Dataset')
-# plt.savefig('./Output/heatmap.png')
-# plt.show()
+df_emotion_totals = pd.DataFrame(emotion_totals_list, columns=df.columns, index=[i+1 for i in range(10)])
+sns.heatmap(df_emotion_totals, annot = True)
+plt.title('Percent Total of Emotions for Video Dataset')
+plt.savefig('./Output/heatmap.png')
+plt.show()
 
 # create stacked percentage chart to compare models
 model_comparison_dict = {'Resnet' : './Output/resnet_frame_info2.csv',
